43d3550c92f4f713c8756f0f6f834c848748c6be
Valen943 <valentinodidio943@gmail.com>
2024-06-18 21:00:08 -0300

    Initial Commit

---
+++ Runtime/Russian_Roullette/Scripts/RussianRoulette.cs
@@ -0,0 +1,44 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class RussianRoulette : UdonSharpBehaviour
+{
+    [SerializeField, Tooltip("1 in x chances of shooting")] int chances = 6;
+    [SerializeField] AudioClip gunShot, emptyShot;
+    [SerializeField, Tooltip("Should the player be teleported on death?")] bool shouldTeleport = true;
+    [SerializeField] Transform TPPoint;
+
+    void Start(){
+        if(TPPoint == null){
+            TPPoint = transform;
+        }
+    }
+
+    public override void OnPickupUseDown()
+    {
+        if (Random.Range(0, chances) == 0){
+            AudioSource.PlayClipAtPoint(gunShot, transform.position);
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Shot");
+            if (shouldTeleport){
+                Networking.LocalPlayer.TeleportTo(TPPoint.position, TPPoint.rotation);
+            }
+        }
+        else{
+            AudioSource.PlayClipAtPoint(emptyShot, transform.position);
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Empty");
+        }
+    }
+
+    public void Shot(){
+        AudioSource.PlayClipAtPoint(gunShot, transform.position);
+    }
+
+    public void Empty(){
+        AudioSource.PlayClipAtPoint(emptyShot, transform.position);
+    }
+
+
+}
