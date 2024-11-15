be2b40e0723fa8401990db4c036325922a0f9b56
Valentino Casadidio <25410859+ElMoha943@users.noreply.github.com>
2024-11-15 20:45:30 -0300

    Update RussianRoulette.cs
    fixed audio

--- Runtime/Russian_Roullette/Scripts/RussianRoulette.cs
+++ Runtime/Russian_Roullette/Scripts/RussianRoulette.cs
@@ -1,44 +1,56 @@
-﻿using UdonSharp;
+using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
-using VRC.Udon;
 
-[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
-public class RussianRoulette : UdonSharpBehaviour
-{
-    [SerializeField, Tooltip("1 in x chances of shooting")] int chances = 6;
-    [SerializeField] AudioClip gunShot, emptyShot;
-    [SerializeField, Tooltip("Should the player be teleported on death?")] bool shouldTeleport = true;
-    [SerializeField] Transform TPPoint;
-
-    void Start(){
-        if(TPPoint == null){
-            TPPoint = transform;
-        }
-    }
-
-    public override void OnPickupUseDown()
+namespace valenvrc.RussianRoulette{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class RussianRoulette : UdonSharpBehaviour
     {
-        if (Random.Range(0, chances) == 0){
-            AudioSource.PlayClipAtPoint(gunShot, transform.position);
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Shot");
-            if (shouldTeleport){
-                Networking.LocalPlayer.TeleportTo(TPPoint.position, TPPoint.rotation);
+        [SerializeField, Tooltip("1 in x chances of shooting")] int chances = 6;
+        [SerializeField] AudioClip gunShot, emptyShot;
+        [SerializeField, Tooltip("Should the player be teleported on death?")] bool shouldTeleport = true;
+        [SerializeField] Transform TPPoint;
+
+        AudioSource audioSource;
+        VRCPlayerApi localPlayer;
+
+        float maxAudioDistance;
+
+        void Start(){
+            audioSource = GetComponent<AudioSource>();
+            if(TPPoint == null){
+                TPPoint = transform;
+            }
+            localPlayer = Networking.LocalPlayer;
+            maxAudioDistance = audioSource.maxDistance;
+        }
+
+        public override void OnPickupUseDown(){
+            if (Random.Range(0, chances) == 0){
+                Shot();
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Shot");
+                if(shouldTeleport){
+                    Networking.LocalPlayer.TeleportTo(TPPoint.position, TPPoint.rotation);
+                }
+            }
+            else{
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Empty");
+                Empty();
             }
         }
-        else{
-            AudioSource.PlayClipAtPoint(emptyShot, transform.position);
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Empty");
+
+        public void Shot(){
+            float playerDistance = Vector3.Distance(localPlayer.GetPosition(), gameObject.transform.position);
+            if (playerDistance < maxAudioDistance){
+                audioSource.PlayOneShot(gunShot);
+            }
+        }
+
+        public void Empty(){
+            float playerDistance = Vector3.Distance(localPlayer.GetPosition(), gameObject.transform.position);
+            if (playerDistance < maxAudioDistance){
+                audioSource.PlayOneShot(emptyShot);
+            }
         }
     }
-
-    public void Shot(){
-        AudioSource.PlayClipAtPoint(gunShot, transform.position);
-    }
-
-    public void Empty(){
-        AudioSource.PlayClipAtPoint(emptyShot, transform.position);
-    }
-
-
 }
