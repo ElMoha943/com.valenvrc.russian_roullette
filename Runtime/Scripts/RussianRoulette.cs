5893f6e540b5464d1916f256426c816a964a8cca
Valentino Casadidio <valentinodidio943@gmail.com>
2026-06-03 23:30:32 -0300

    Updated pickup to 1.1 version, updated to use networkcalling, set min value of chances to 1.

--- Runtime/Scripts/RussianRoulette.cs
+++ Runtime/Scripts/RussianRoulette.cs
@@ -1,15 +1,17 @@
 ﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDK3.Components;
+using VRC.SDK3.UdonNetworkCalling;
 using VRC.SDKBase;
+using VRC.Udon.Common.Interfaces;
 
 namespace valenvrc.RussianRoulette{
-    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync),Icon("Packages/com.valenvrc.russian_roullette/Editor/Resources/RevolverIcon.png"), HelpURL("https://discord.gg/nv5ax3wDqc")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync),Icon("Packages/com.valenvrc.russian_roullette/Editor/Resources/RevolverIcon.png"), HelpURL("https://docs.valenvrc.com/free-assets/russian-roulette")]
     [RequireComponent(typeof(AudioSource))]
     [RequireComponent(typeof(VRCPickup))]
     public class RussianRoulette : UdonSharpBehaviour
     {
-        [SerializeField, Tooltip("1 in x chances of shooting")] int chances = 6;
+        [SerializeField, Tooltip("1 in x chances of shooting. 1 means it will always shoot, 2 means 50/50, etc."), Min(1)] int chances = 6;
         [SerializeField] AudioClip gunShot, emptyShot;
         [SerializeField, Tooltip("Should the player be teleported on death?")] bool shouldTeleport = true;
         [SerializeField] Transform TPPoint;
@@ -30,16 +32,17 @@ namespace valenvrc.RussianRoulette{
 
         public override void OnPickupUseDown(){
             if (Random.Range(0, chances) == 0){
-                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Shot");
+                NetworkCalling.SendCustomNetworkEvent((IUdonEventReceiver)this, NetworkEventTarget.All, "Shot");
                 if (shouldTeleport){
                     Networking.LocalPlayer.TeleportTo(TPPoint.position, TPPoint.rotation);
                 }
             }
             else{
-                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Empty");
+                NetworkCalling.SendCustomNetworkEvent((IUdonEventReceiver)this, NetworkEventTarget.All, "Empty");
             }
         }
 
+        [NetworkCallable]
         public void Shot(){
             float playerDistance = Vector3.Distance(localPlayer.GetPosition(), gameObject.transform.position);
             if (playerDistance < maxAudioDistance){
@@ -47,6 +50,7 @@ namespace valenvrc.RussianRoulette{
             }
         }
 
+        [NetworkCallable]
         public void Empty(){
             float playerDistance = Vector3.Distance(localPlayer.GetPosition(), gameObject.transform.position);
             if (playerDistance < maxAudioDistance){
